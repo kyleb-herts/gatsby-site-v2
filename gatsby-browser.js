7342b86665525093ba535232a2393752214d6366
kyleb-herts <81439202+kyleb-herts@users.noreply.github.com>
2021-10-02 21:17:56 +0100

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "aos/dist/aos.css" 
+import "./src/styles/main.css"
+
---
+++ gatsby-config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  siteMetadata: {
+    title: `Kerbobble | We produce digital products for businesses.`,
+    description: `We are Kerbobble and we produce applications for businesses. We believe in designing quality applications that go beyond, that everyone can be proud of.`,
+    author: `@Kerbobble`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/images/uploads`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/pages/`,
+      },
+    },
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`300`, `400`, `500`, `600`, `700`]
+          },
+        ],
+      },
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
